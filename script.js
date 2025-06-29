160052d741a808d0246678f40d0fbe6dc7dc64c4
Gabriel Vieira <gabrielvieiradev.tech@gmail.com>
2025-06-28 21:55:59 -0300

    Criando o arquivo README.md

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.querySelector('.btn');
+const mensagem = document.querySelector('#mensagem')
+botao.addEventListener('click', () => {
+    mensagem.innerHTML = "Obrigado por visitar meu site! Nunca desista dos seus sonhos! 🚀"
+});
